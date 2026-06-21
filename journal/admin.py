d3e487953a5a1548917d67e7536bb3c29bd48163
AjesBatie <andsdigitech@gmail.com>
2026-06-21 22:27:49 +0100

    Fix: Correction des noms d'importation JournalPost et JournalPostMedia dans l'admin

--- journal/admin.py
+++ journal/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from .models import JournalPost, JournalPostLike
+from .models import JournalPost, JournalPostMedia, JournalPostLike, JournalPostComment
 
+class JournalPostMediaInline(admin.TabularInline):
+    model = JournalPostMedia
+    extra = 3  # Te donne 3 cases par défaut pour ajouter des photos/vidéos d'un coup
 
 @admin.register(JournalPost)
 class JournalPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'event_date', 'is_published')
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('title', 'category', 'is_published', 'created_at')
+    inlines = [JournalPostMediaInline]  # Lie le système multi-images/vidéos au post
 
-
-@admin.register(JournalPostLike)
-class JournalPostLikeAdmin(admin.ModelAdmin):
-    list_display = ('post', 'user', 'created_at')
-    search_fields = ('post__title', 'user__username')
+admin.site.register(JournalPostLike)
+admin.site.register(JournalPostComment)
